ee975077b97a236746e948616489814d43209346
ratnanil <rata@zhaw.ch>
2025-10-12 23:54:59 +0200

    feat: Introduce and standardize 'zones' raster example across lessons

--- one-time/get-data.R
+++ one-time/get-data.R
@@ -3,3 +3,11 @@
 data(Produc, package = "plm")
 
 readr::write_csv(Produc, "data/Produc.csv")
+
+
+
+
+f <- system.file("ex/lux.shp", package="terra")
+v <- vect(f)
+r <- rast(system.file("ex/elev.tif", package="terra"))
+zones <- rasterize(v, r, "NAME_2")
